82d873cae2abc86b5b8b8a3e7af10f2c4b17c7f4
Ronit halder <ronit.crj@gmail.com>
2023-03-03 21:18:50 +0530

    use unique_ptr

--- src/test.cpp
+++ src/test.cpp
@@ -1,12 +1,14 @@
 #include <string>
+#include <iostream>
 
 #include <eviction.hpp>
 #include <cache.hpp>
-
+#include <memory>
 
 int main() {
-	cache<lru_eviction> c (40);
+	std::unique_ptr<cache<lru_eviction>> c = std::make_unique<cache<lru_eviction>>(10);
 	std::string val;
-	c.put(4, val);
-	c.get(4, val);
+	c->put(4, "e");
+	c->get(4, val);
+	std::cout<<val<<std::endl;
 }
